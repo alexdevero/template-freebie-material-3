feaa05f4b978cc110020857d9b1e0921780c8adc
Alex Devero <deveroalex@gmail.com>
2018-12-23 07:25:19 +0100

    Update layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,54 +1,29 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
 
+import { Footer } from './footer'
 import { Header } from './header'
 
-import '../styles/styles.css'
+const Layout = ({ children, pageTitle }) => (
+  <>
+    <Helmet>
+      <title>{pageTitle} | Material Template 3.0</title>
+    </Helmet>
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet>
-          {/* Roboto typeface */}
-          <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet" />
+    <div className="wrapper">
+      <Header />
 
-          {/* Material icons */}
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      {children}
 
-          {/* Font Aweseome icons */}
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous" />
-        </Helmet>
-
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+      <Footer />
+    </div>
+  </>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  pageTitle: PropTypes.string
 }
 
 export default Layout
