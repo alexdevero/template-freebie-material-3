07bd0d4fde79e982289a4699cb9f4cc4a2dd66d3
Alex Devero <deveroalex@gmail.com>
2018-12-29 08:02:32 +0100

    Add styleguide page

---
+++ src/pages/styleguide.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+class StyleguidePage extends React.PureComponent {
+  state = {
+    isProActive: false
+  }
+
+  handleProSwitch = () => {
+    this.setState({
+      isProActive: !this.state.isProActive
+    })
+  }
+
+  render () {
+    return(
+      <Layout>
+        <section className="hero">
+          <h1>Material 3.0 </h1>
+
+          <h2>All components</h2>
+
+          <div className="custom-control custom-switch" onClick={this.handleProSwitch}>
+            <input type="checkbox" className="custom-control-input" id="customSwitch1" />
+
+            <label className="custom-control-label" for="customSwitch1">Show PRO version</label>
+          </div>
+        </section>
+
+        <section>
+          <h1>Basic elements</h1>
+        </section>
+
+        <section>
+          <h1>Navigation</h1>
+        </section>
+
+        <section>
+          <h1>Notifications</h1>
+        </section>
+
+        <section>
+          <h1>Footers</h1>
+        </section>
+
+        <section>
+          <h1>Typography</h1>
+        </section>
+
+        <section>
+          <h1>Content Areas</h1>
+        </section>
+
+        <section>
+          <h1>Cards</h1>
+        </section>
+
+        <section>
+          <h1>Morphing Cards</h1>
+        </section>
+
+        <section>
+          <h1>JavaScript</h1>
+        </section>
+      </Layout>
+    )
+  }
+}
+
+export default StyleguidePage
