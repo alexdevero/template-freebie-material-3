c443e85d0babe76f9a5f51f9b4c8080645ce6dcf
Alex Devero <deveroalex@gmail.com>
2018-12-29 07:01:07 +0100

    Remove deleted seo component

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import SEO from '../components/seo'
 
 const Portfolio = () => (
   <Layout>
-    <SEO title="Portfolio" />
     <h1>Hi from the Portfolio</h1>
+
     <p>Welcome to page 2</p>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
--- src/pages/services.js
+++ src/pages/services.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import SEO from '../components/seo'
 
 const Services = () => (
   <Layout>
-    <SEO title="Services" />
     <h1>Hi from the Services</h1>
+
     <p>Welcome to page 2</p>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
