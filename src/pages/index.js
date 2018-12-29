a95c405c7ebefca1fb154a947f179792ea5eba96
Alex Devero <deveroalex@gmail.com>
2018-12-29 06:59:36 +0100

    Add example pages

---
+++ src/pages/index.js
@@ -0,0 +1,575 @@
+import React from 'react'
+
+import { Hero } from '../components/hero'
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout pageTitle="Home">
+    <Hero type="large">
+      <h1 className="h4">Welcome to Material</h1>
+
+      <p>Start the development with this awesome Bootstrap 4 UI Kit inspired by Material Design.</p>
+    </Hero>
+
+    <section className="features">
+      <div className="container">
+        <p>Material UI Kit is a Bootstrap UI Kit with a fresh, new design inspired by Google's Material Design. You asked for it, so we built it. It's a great pleasure to introduce to you the material concepts in an easy to use and beautiful set of components.</p>
+        <div className="row">
+          <div className="col-md-4">
+            <img src="" alt="" className="features__icon" />
+
+            <h2 className="features__heading h3">Incredibly Number of Components</h2>
+
+            <p className="features__slug">Every element that you need in a product comes built in as a component. All components fit perfectly with each other and can take variations in colour.</p>
+          </div>
+          <div className="col-md-4">
+            <img src="" alt="" className="features__icon" />
+
+            <h2 className="features__heading h3">Multi-Purpose Sections</h2>
+
+            <p className="features__slug">Putting together a page has never been easier than matching together sections. From team presentation to pricing options, you can easily customise and built your pages.</p>
+          </div>
+          <div className="col-md-4">
+            <img src="" alt="" className="features__icon" />
+
+            <h2 className="features__heading h3">Pixel-Perfect Example Pages</h2>
+
+            <p className="features__slug">If you want to get inspiration or just show something directly to your clients, you can jump start your development with our pre-built example pages.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h2 className="h2">Basic Components</h2>
+
+            <h3 className="h3">The core elements of your website</h3>
+
+            <p>We re-styled every Bootstrap element to make it resemble Material Design and also fit with each other. All the Bootstrap components that you need in a development have been re-design with the new look. Besides the numerous basic elements, we have also created additional classes. All these items will help you take your project to the next level.</p>
+          </div>
+          <div className="col-md-6">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src="" alt="" />
+          </div>
+          <div className="col-md-6">
+            <h2 className="h2">Unconventional Cards</h2>
+
+            <h3 className="h3">One card for every problem</h3>
+
+            <p>We love cards and everybody on the web seems to. We have gone above and beyond with options for you to organise your information. From cards designed for blog posts, to product cards or user profiles, you will have many options to choose from. All the cards follow the material principles and have a design that stands out.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h2 className="h2">Content Areas</h2>
+
+            <h3 className="h3">For areas that need more space</h3>
+
+            <p>If you need elements such as tables, comments, description areas, tabs and many others, we've got you covered. We took into consideration multiple use cases and came up with some specific elements that you will love to use. They're beautiful and easy to use for the end user navigating your website.</p>
+          </div>
+          <div className="col-md-6">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div className="container">
+        <h1>Ready to Use Sections</h1>
+
+        <p>Build your page section by section! We have created multiple options for you to put together and customise into pixel perfect pages. From headers to footers, you will be able to choose the best combination for your project.</p>
+
+        <a href="#!" className="btn">
+          View all sections
+        </a>
+      </div>
+    </section>
+
+    <div className="gallery gallery--fluid">
+      <div className="contaner-fluid">
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+
+        <div className="col-md-3">
+          <a href="#!">
+            <img src="" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <section className="gallery gallery--masonry">
+      <div className="container">
+        {/*
+         * React plugins:
+         * https://www.npmjs.com/package/react-masonry-layout
+         */}
+
+        <h1 className="h2">Example Pages</h1>
+
+        <p>The easiest way to get started is to use one of our pre-built example pages. From landing pages to e-commerce or blog pages, you will be able jump start your development. Show your clients a quick prototype and get inspired for your next project!</p>
+
+        <div>
+          <h2 className="h3">About Us</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Landing Page</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Contact Us</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Blog Post</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Product Page</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Login Page</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Pricing Page</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Signup Page</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Profile Page</h2>
+
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <h2 className="h3">Blog Posts</h2>
+
+          <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+
+    <section className="demo">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="h2">Free Demo</h1>
+
+            <p>Do you want to test and see the benefits of this kit before purchasing it? You can give the demo version a try. It features enough basic components for you to get a feel of the design and also test the quality of the code. Get it free on GitHub!</p>
+
+            <a href="#!" className="btn">
+              View demo on GitHub
+            </a>
+          </div>
+
+          <div className="col-md-6">
+            <div className="col-md-6">
+              <h2 className="h2">Free Demo</h2>
+
+              <ul>
+                <li>20 Components</li>
+                <li>3 Example Pages</li>
+                <li>
+                  <span className="fa fa-times" /> Unconventional Cards
+                </li>
+                <li>
+                  <span className="fa fa-times" /> Sections
+                </li>
+                <li>
+                  <span className="fa fa-times" /> Premium Support
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6">
+              <h2 className="h2">PRO Version</h2>
+
+              <ul>
+                <li>60+ Components</li>
+                <li>10 Example Pages</li>
+                <li>
+                  <span className="fa fa-check" /> Unconventional Cards
+                </li>
+                <li>
+                  <span className="fa fa-check" /> Sections
+                </li>
+                <li>
+                  <span className="fa fa-check" /> Premium Support
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="benefits">
+      <div className="container">
+        <h1 className="h2">Your work will get much easier</h1>
+        <div className="row">
+          <div className="col-md-3">
+            <img src="" alt="" className="benefits__icon" />
+
+            <h2 className="benefits__heading h3">Bootstrap Grid</h2>
+
+            <p className="benefits__slug">Enjoy the fluid grid system based on 12 columns. Material Kit PRO is built over Bootstrap and has all the benefits that the framework comes with.</p>
+          </div>
+          <div className="col-md-3">
+            <img src="" alt="" className="benefits__icon" />
+
+            <h2 className="benefits__heading h3">Fully Responsive</h2>
+
+            <p className="benefits__slug">This Bootstrap kit is built mobile-first and looks amazing on any device. Regardless of the screen size, the website content will naturally fit the given resolution.</p>
+          </div>
+          <div className="col-md-3">
+            <img src="" alt="" className="benefits__icon" />
+
+            <h2 className="benefits__heading h3">Save Time</h2>
+
+            <p className="benefits__slug">Using the Material Kit PRO will save you large amount of time. You don't have to worry about customising the basic Bootstrap design or generating new components.</p>
+          </div>
+          <div className="col-md-3">
+            <img src="" alt="" className="benefits__icon" />
+
+            <h2 className="benefits__heading h3">Save Money</h2>
+
+            <p className="benefits__slug">Creating your design from scratch with dedicated designers can be very expensive. Using a kit is the best option to start your development while bootstrapping your budget.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="testimonials">
+      <div className="container">
+        <h1 className="h2">Trusted by 1.000+ People</h1>
+        <p>This UI Kit, has been used and tested by 1,000+ web developers and webdesigners in over 10.000 Web Projects. This is what some of them say:</p>
+        <div className="row">
+          <div className="col-md-4">
+            <img src="" alt="" className="testimonials__avatar" />
+
+            <h2 className="testimonials__text h3">Sergey Vlademov</h2>
+
+            <p className="testimonials__text">"As soon as I saw this kit, everything else isn't the same anymore, I just can't describe it guys! Thank you for this work!"</p>
+          </div>
+          <div className="col-md-4">
+            <img src="" alt="" className="testimonials__avatar" />
+
+            <h2 className="testimonials__text h3">Steven Bauman</h2>
+
+            <p className="testimonials__text">"Great kit! Used this for a client already and he is over the moon. Keep up the good work Creative Tim! 10/10 for design, colours, and the feel of the kit."</p>
+          </div>
+          <div className="col-md-4">
+            <img src="" alt="" className="testimonials__avatar" />
+
+            <h2 className="testimonials__text h3">Sunjay Pital</h2>
+
+            <p className="testimonials__text">"Damn Alex!!! This Material UI Kit is fresh AF! Amazing work!"</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="cta">
+      <div className="container">
+        <h1 className="h2">Ready to get your Material UI Kit Pro?</h1>
+
+        <div className="switch">
+          <label htmlFor="" className="switch__label">
+            Personal license
+          </label>
+
+          <input type="checkbox" className="switch__checkbox" />
+
+          <label htmlFor="" className="switch__label">
+            Developer license
+          </label>
+        </div>
+
+        {/*this.state.isPersonal ? (
+          <p>Good for business projects, multiple websites for clients or for creating themes where your users will be charged for using it. Create multiple websites/apps for multiple clients.</p>
+        ) : (
+          <p>Good for personal websites, school projects, startups or any other project where your users will not be charged for using it. Create one single website/app for one client.</p>
+        )*/}
+
+        <div className="row">
+          <div className="col-md-6">
+            <div className="plan plan--lite">
+              <h2 className="h3">HTML Package</h2>
+
+              <h3 className="h4">{/*this.state.isPersonal ? '$79' : '$229'*/}</h3>
+
+              <ul>
+                <li>HTML/CSS/JS Files</li>
+                <li>Fully Coded Components</li>
+                <li>Responsive Design</li>
+                <li>Product Documentation</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="plan plan--full">
+              <h2 className="h3">HTML + React Package</h2>
+
+              <h3 className="h4">{/*this.state.isPersonal ? '$139' : '¨$599'*/}</h3>
+
+              <ul>
+                <li>HTML/CSS/JS/React Files</li>
+                <li>Fully Coded Components</li>
+                <li>Responsive Design</li>
+                <li>Product Documentation</li>
+                <li>+ React version</li>
+                <li>+ Gatsbyjs version</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p>You have Free Unlimited Updates and limited premium support on each package. You also have 30 days to request a refund if you're not happy with your purchase.</p>
+    </section>
+
+    <div className="share">
+      <ul>
+        <li>
+          <a href="#!" className="btn">
+            <span className="fa fa-twitter" />
+          </a>
+        </li>
+        <li>
+          <a href="#!" className="btn">
+            <span className="fa fa-facebook" />
+          </a>
+        </li>
+        <li>
+          <a href="#!" className="btn">
+            <span className="fa fa-pinterest" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
