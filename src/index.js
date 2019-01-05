4660c247ab8e3547c987450aa7bd46d2b61cfebb
Alex Devero <deveroalex@gmail.com>
2019-01-05 15:24:59 +0100

    Prepare imports and routes for subpages

--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import * as serviceWorker from './serviceWorker'
 
+// Import pages
+// import { 404 } from './pages/404'
+// import { About } from './pages/about'
+// import { BlogPost } from './pages/blog-post'
+// import { Blog } from './pages/blog'
+// import { Contact } from './pages/contact'
 import IndexPage from './pages/index'
+// import { Portfolio } from './pages/portfolio'
+// import { Services } from './pages/services'
 import StyleguidePage from './pages/styleguide'
 
+// Import Presentation Panel for
+// import PresentationPanel from './components/presentation-panel'
+
+// Import components
+
+// Import main CSS stylesheet
 import './styles/styles.css'
 
 class App extends React.Component {
@@ -14,8 +28,17 @@ class App extends React.Component {
     return(
       <BrowserRouter>
         <React.Fragment>
+          {/* <PresentationPanel /> */}
+
           <Route path="/" component={IndexPage} exact={true} />
 
+          {/* <Route path="/404" component={404} /> */}
+          {/* <Route path="/about" component={About} /> */}
+          {/* <Route path="/blogPost" component={BlogPost} /> */}
+          {/* <Route path="/blog" component={Blog} /> */}
+          {/* <Route path="/contact" component={Contact} /> */}
+          {/* <Route path="/portfolio" component={Portfolio} /> */}
+          {/* <Route path="/services" component={Services} /> */}
           <Route path="/styleguide" component={StyleguidePage} />
         </React.Fragment>
       </BrowserRouter>
