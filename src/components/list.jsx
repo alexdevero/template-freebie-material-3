37e26c4d4a5085b4f7d6ff1eebae9513e063bcdf
Alex Devero <deveroalex@gmail.com>
2019-01-23 07:42:50 +0100

    Create component for lists

---
+++ src/components/list.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export class List extends React.PureComponent {
+  generateClasses = () => {
+    return this.props.className !== undefined ? this.props.className : null
+  }
+
+  getType = () => {
+    let type
+
+    switch (this.props.type) {
+      case 'inline':
+        type = `${this.props.className !== undefined ? ' list-inline' : 'list-inline'}`
+        break
+
+      case 'unstyled':
+        type = `${this.props.className !== undefined ? ' list-unstyled' : 'list-unstyled'}`
+        break
+
+      default:
+        type = null
+        break
+    }
+
+    return type
+  }
+
+  render() {
+    return <ul className={[this.generateClasses(), this.getType()].join('')}>
+      {this.props.children}
+    </ul>
+  }
+}
+
+List.propTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(['inline', 'unstyled']).isRequired
+}
