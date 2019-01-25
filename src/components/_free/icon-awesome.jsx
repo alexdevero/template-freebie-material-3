ce779fbd658d383e4acc55a88beef5f8e506125e
Alex Devero <deveroalex@gmail.com>
2019-01-25 07:27:04 +0100

    Fix Awesome icon classes

--- src/components/_free/icon-awesome.jsx
+++ src/components/_free/icon-awesome.jsx
@@ -9,40 +9,40 @@ export class IconAwesome extends React.PureComponent {
 
     switch (this.props.size) {
       case 'xs':
-        iconSize = 'fa-xs'
+        iconSize = ' fa-xs'
         break;
       case 'sm':
-        iconSize = 'fa-sm'
+        iconSize = ' fa-sm'
         break;
       case 'lg':
-        iconSize = 'fa-lg'
+        iconSize = ' fa-lg'
         break;
       case '2x':
-        iconSize = 'fa-2x'
+        iconSize = ' fa-2x'
         break;
       case '3x':
-        iconSize = 'fa-3x'
+        iconSize = ' fa-3x'
         break;
       case '4x':
-        iconSize = 'fa-4x'
+        iconSize = ' fa-4x'
         break;
       case '5x':
-        iconSize = 'fa-5x'
+        iconSize = ' fa-5x'
         break;
       case '6x':
-        iconSize = 'fa-6x'
+        iconSize = ' fa-6x'
         break;
       case '7x':
-        iconSize = 'fa-7x'
+        iconSize = ' fa-7x'
         break;
       case '8x':
-        iconSize = 'fa-8x'
+        iconSize = ' fa-8x'
         break;
       case '9x':
-        iconSize = 'fa-9x'
+        iconSize = ' fa-9x'
         break;
       case '10x':
-        iconSize = 'fa-10x'
+        iconSize = ' fa-10x'
         break;
       default:
         iconSize = ''
@@ -52,7 +52,7 @@ export class IconAwesome extends React.PureComponent {
   }
 
   render() {
-    return <span className={this.generateSize() + `fa${this.props.type.substring(0, 1).toLowerCase()} fa-${this.props.name} ${this.props.className !== undefined ? this.props.className : ''}`} aria-hidden={true} />
+    return <span className={`fa${this.props.type.substring(0, 1).toLowerCase()} fa-${this.props.name}` + this.generateSize() + (this.props.className !== undefined ? ' ' + this.props.className : '')} aria-hidden={true} />
   }
 }
 
