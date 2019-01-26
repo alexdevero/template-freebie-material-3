c21a3b4a87867e354d4186cda8a6a17ddc0714ba
Alex Devero <deveroalex@gmail.com>
2019-01-26 08:18:05 +0100

    Update material icon component

--- src/components/_free/icon-material.jsx
+++ src/components/_free/icon-material.jsx
@@ -7,7 +7,7 @@ export class IconMaterial extends React.PureComponent {
   generateSize = () => {
     let iconSize = ''
 
-    switch (this.props.size) {
+    /* switch (this.props.size) {
       case '18':
         iconSize = 'material-icons-18 '
         break
@@ -20,6 +20,47 @@ export class IconMaterial extends React.PureComponent {
       case '48':
         iconSize = 'material-icons-48 '
         break
+      default:
+        iconSize = ''
+    } */
+
+    switch (this.props.size) {
+      case 'xs':
+        iconSize = ' material-icons-xs'
+        break;
+      case 'sm':
+        iconSize = ' material-icons-sm'
+        break;
+      case 'lg':
+        iconSize = ' material-icons-lg'
+        break;
+      case '2x':
+        iconSize = ' material-icons-2x'
+        break;
+      case '3x':
+        iconSize = ' material-icons-3x'
+        break;
+      case '4x':
+        iconSize = ' material-icons-4x'
+        break;
+      case '5x':
+        iconSize = ' material-icons-5x'
+        break;
+      case '6x':
+        iconSize = ' material-icons-6x'
+        break;
+      case '7x':
+        iconSize = ' material-icons-7x'
+        break;
+      case '8x':
+        iconSize = ' material-icons-8x'
+        break;
+      case '9x':
+        iconSize = ' material-icons-9x'
+        break;
+      case '10x':
+        iconSize = ' material-icons-10x'
+        break;
       default:
         iconSize = ''
     }
@@ -62,7 +103,7 @@ export class IconMaterial extends React.PureComponent {
   }
 
   render() {
-    return <span className={this.generateSize() + this.generateStyle() + this.generateActivity() + `material-icons${this.props.className !== undefined ? ` ${this.props.className}` : ''}`} aria-hidden={true}>
+    return <span className={`material-icons${this.props.className !== undefined ? ` ${this.props.className}` : ''}` + this.generateStyle() + this.generateSize() + this.generateActivity()} aria-hidden={true}>
       {this.props.name}
     </span>
   }
@@ -72,6 +113,7 @@ IconMaterial.propTypes = {
   active: PropTypes.bool,
   className: PropTypes.string,
   name: PropTypes.string.isRequired,
-  size: PropTypes.oneOf(['18', '24', '36', '48']),
+  // size: PropTypes.oneOf(['18', '24', '36', '48']),
+  size: PropTypes.oneOf(['xs', 'sm', 'lg', '2x', '3x', '4x', '5x', '6x', '7x', '8x', '9x', '10x']),
   style: PropTypes.oneOf(['dark', 'light'])
 }
