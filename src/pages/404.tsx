31b67a2c46fbdfb93f14bf57b6175140831e2973
Alex Devero <deveroalex@gmail.com>
2019-02-06 06:28:20 +0100

    Add content for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-import Layout from '../components/layout'
+import { GridRow } from '../components/grid-row'
+import { GridColumn } from '../components/grid-column'
+import { Layout } from '../components/layout'
+import { Section } from '../components/section'
 
 const NotFoundPage = () => (
   <Layout pageTitle="404">
-    <h1>NOT FOUND</h1>
+    <Section container="default" className="d-flex align-items-center">
+      <GridRow className="justify-content-center text-center">
+        <GridColumn sm={10} md={8} lg={6}>
+          <h1 className="h1">&#123; 404 &#125;</h1>
 
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <h2 className="h2 mt-4 mb-4">... Not found ...</h2>
+
+          <p className="mb-4">You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+          <Link to="/">Back home</Link>
+        </GridColumn>
+      </GridRow>
+    </Section>
   </Layout>
 )
 
